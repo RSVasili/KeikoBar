48e391347dbee15d07d68d49d3b98ebc72d164b0
RSVasili <Wassart@gmail.com>
2022-10-09 22:35:37 +0300

    Start project

---
+++ Keikobar/Keikobar/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Keikobar.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Keikobar.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
